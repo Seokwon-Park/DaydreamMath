5ec985978815d2d69e53d0e589c166d99863153b
ParkSW <rty3394@naver.com>
2026-04-17 12:18:04 +0900

    오류수정

--- DaydreamMath/Vector3Impl.h
+++ DaydreamMath/Vector3Impl.h
@@ -15,7 +15,7 @@ namespace Daydream
 	Vector<3, T> Vector<3, T>::Rotate(const Quat<T>& _quat, const Vector<3, T> _vector)
 	{
 		Vector<3, T> qVec(_quat.x, _quat.y, _quat.z);
-		Vector<3, T> t = Cross(qVec, _v) * static_cast<T>(2.0);
+		Vector<3, T> t = Cross(qVec, _vector) * static_cast<T>(2.0);
 		return _vector + (t * _quat.w) + Cross(qVec, t);
 	}
 }
